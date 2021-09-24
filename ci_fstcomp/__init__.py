84e5a9ef14c310743fead3e9d961654d4cbc674c
Sébastien Fortier <sebastien.fortier@canada.ca>
2021-09-24 11:24:25 -0400

    bump vrsion and add 3.8 version of lib

--- ci_fstcomp/__init__.py
+++ ci_fstcomp/__init__.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
+import os
+from pathlib import Path
+
 from .fstcomp import *
 from .dataframe import *
 from .std_io import *
 from .fstcompstats import *
+
+
+p = Path(os.path.abspath(__file__))
+v_file = open(p.parent.parent / 'VERSION')
+__version__ = v_file.readline().strip()
+v_file.close()
